23c31768cd703afcd1f47648ecd6350f0845eb39
Mikko Haavisto <mikko.haavisto@tuni.fi>
2023-12-27 12:53:36 +0200

    Techically working but apparently too slow for part 2 (2022)

--- 2022/11_Monkey_in_the_Middle.py
+++ 2022/11_Monkey_in_the_Middle.py
@@ -7,10 +7,49 @@ from typing import List, Callable
 class Item:
     worryLevel: int
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return str(self.worryLevel)
 
+class MonkeyHerd:
+    def __init__(self):
+        self.monkeys = []
 
+    def add_monkey(self, Monkey):
+        self.monkeys.append(Monkey)
+    
+    def remove_monkey(self, id):
+        self.monkeys = [monkey for monkey in self.monkeys if monkey.id != id]
+    
+    def toss(self, tosserId : int, targetId : int, item : Item):
+        try:
+            tosser = [m for m in self.monkeys if m.id == tosserId][0]
+        except IndexError:
+            print(f"Tosser with id {tosserId} not found")
+
+        try:
+            target = [m for m in self.monkeys if m.id == targetId][0]
+        except IndexError:
+            print(f"Target with id {targetId} not found")
+        
+        tosser.startingItems = [i for i in tosser.startingItems if i.worryLevel != item.worryLevel]
+        target.startingItems.append(item)
+
+    def round(self, n=1, verbose=False, extraWorry=False):
+        for i in range(n):
+            for monkey in self.monkeys:
+                monkey.turn(verbose, extraWorry)
+
+    def monkeyBusiness(self):
+        # multiply two most active monkey's inspect times together
+        businessLevels = [monkey.inspectedItems for monkey in self.monkeys]
+        total = 1
+        print(sorted(businessLevels))
+        for b in sorted(businessLevels)[-2:]:
+            total *= b
+        return total
+            
+
+@dataclass
 class Monkey:
     id              : int
     startingItems   : List[Item]
@@ -18,18 +57,26 @@ class Monkey:
     test            : Callable[[Item], bool]
     testTrueTarget  : int 
     testFalseTarget : int
+    herd : MonkeyHerd
+    inspectedItems : int = 0
 
-
-
-""" !!!
-After each monkey inspects an item 
-but before it tests your worry level, 
-your relief that the monkey's inspection didn't damage the item 
-causes your worry level to be 
-divided by three and 
-rounded down to the nearest integer.
-"""
-
+    def turn(self, verbose=False, extraWorry=False):
+        if verbose: print(f'Monkey {self.id}:')
+        for item in self.startingItems:
+            if verbose:
+                print(f"    Monkey inspects an item with a worry level of {item.worryLevel}.")
+            self.inspectedItems += 1
+            item.worryLevel = self.operation(item.worryLevel)
+            if not extraWorry:
+                if verbose: print(f'        Worry level increases to {item.worryLevel}')
+                item.worryLevel = item.worryLevel // 3
+            if verbose: print(f'        Monkey gets bored with item. Worry level is divided by 3 to {item.worryLevel}')
+            if self.test(item):
+                if verbose: print(f'        Item with worry level {item.worryLevel} is thrown to monkey {self.testTrueTarget}')
+                self.herd.toss(self.id, self.testTrueTarget, item)
+            else:
+                if verbose: print(f'        Item with worry level {item.worryLevel} is thrown to monkey {self.testFalseTarget}')
+                self.herd.toss(self.id, self.testFalseTarget, item)
 
 
 def parseStartingItems(x : str) -> List[int]:
@@ -93,10 +140,15 @@ def parseTest(x: str) -> Callable[[Item], bool]:
     return test
 
 
+def parseTarget(x: str) -> int:
+    try:
+        return int(x.split()[-1].strip())
+    except ValueError:
+        print(f"Invalid target: {x.split()[-1]}")
 
 
 def read_monkeys_from_file(filename):
-    monkeys = []
+    herd = MonkeyHerd()
     with open(filename, 'r') as f:
         monkeyId = None
         startingItems = None
@@ -114,7 +166,7 @@ def read_monkeys_from_file(filename):
             body = parts[1].strip()
 
             if head.startswith('Monkey'):
-                monkeyId = head.split()[1]
+                monkeyId = int(head.split()[1])
                 print(f"{monkeyId=}")
             elif head.startswith('Starting items'):
                 startingItems = parseStartingItems(body)
@@ -127,7 +179,17 @@ def read_monkeys_from_file(filename):
                 test = parseTest(body)
                 print(test)
                 print(test(Item(23)))
+            elif head.startswith('If true'):
+                testTrueTarget = parseTarget(body)
+                print(testTrueTarget)
+            elif head.startswith('If false'):
+                testFalseTarget = parseTarget(body)
+                print(testFalseTarget)
+                # if false is the last row of monkey definition
+                # -> at this point we create monkey 
+                herd.add_monkey(Monkey(monkeyId, startingItems, operation, test, testTrueTarget, testFalseTarget, herd))
 
+    return herd
 
 
 
@@ -135,8 +197,18 @@ def read_monkeys_from_file(filename):
 
 
 def main():
-    read_monkeys_from_file('inputs/11_example.txt')
-    #read_monkeys_from_file('inputs/11_input.txt')
+    herd = read_monkeys_from_file('inputs/11_example.txt')
+    # herd = read_monkeys_from_file('inputs/11_input.txt')
+
+    # for r in range(3):
+    #     print(f"\n*** ROUND {r+1} ***")
+    #     herd.round(verbose=False)
+    #     [print(f'{m.id} | {m.inspectedItems} | {m.startingItems}') for m in herd.monkeys]
+
+    for r in range(10000):
+        herd.round(extraWorry=True)
+        print(f"{r:5} | {herd.monkeyBusiness()}")
+    
 
 
 
